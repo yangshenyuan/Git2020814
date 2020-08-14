d3fad49d8296fee84c5655e765da0e0a74764796
yangshenyuan <2770456261@qq.com>
2020-08-14 16:26:31 +0800

    删除部分文件

--- src/main/java/com/lanxin/CustomRealmApp.java
+++ src/main/java/com/lanxin/CustomRealmApp.java
@@ -13,9 +13,6 @@ import org.apache.shiro.subject.Subject;
 //3.3验证 （自定义Realm）
 public class CustomRealmApp {
     private String aaa;
-    private String bbb;
-
-    private String ccc;
     public static void main(String[] args) {
 
         CustomRealm customRealm=new CustomRealm();
