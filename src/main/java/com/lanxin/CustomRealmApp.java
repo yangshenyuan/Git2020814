6825e6a382290698e4406bf8a34e10c8021f95ca
yangshenyuan <2770456261@qq.com>
2020-08-14 16:22:21 +0800

    修改了部分代码

--- src/main/java/com/lanxin/CustomRealmApp.java
+++ src/main/java/com/lanxin/CustomRealmApp.java
@@ -12,10 +12,7 @@ import org.apache.shiro.subject.Subject;
 ////
 //3.3验证 （自定义Realm）
 public class CustomRealmApp {
-    private String aaa;
-    private String bbb;
 
-    private String ccc;
     public static void main(String[] args) {
 
         CustomRealm customRealm=new CustomRealm();
