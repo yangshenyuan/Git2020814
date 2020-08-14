6784392df307710c2d9c7862c1a7d8b594cb8415
git <1105561200@qq.com>
2020-08-14 16:31:58 +0800

    rtrtr

--- src/main/java/com/lanxin/CustomRealmApp.java
+++ src/main/java/com/lanxin/CustomRealmApp.java
@@ -13,6 +13,7 @@ import org.apache.shiro.subject.Subject;
 //3.3验证 （自定义Realm）
 public class CustomRealmApp {
     private String ddd;
+    private String aaaa;
     public static void main(String[] args) {
 
         CustomRealm customRealm=new CustomRealm();
